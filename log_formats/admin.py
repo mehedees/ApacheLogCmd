a68fb27d28b9ddbebf5833a8548e24809805ddbd
Mehedee Ahmed Siddique <siddique@divine-it.net>
2018-11-19 13:03:55 +0600

    Deleted unnecessary(duplicate)files and folders

--- log_formats/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import LogFormats
-
-
-admin.site.register(LogFormats)
--- log_formats/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class LogFormatsConfig(AppConfig):
-    name = 'log_formats'
